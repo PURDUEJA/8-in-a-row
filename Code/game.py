1aa05f6aa69c6f62d725a263f3cd307fccd90644
NickK-coding <83576368+NickK-coding@users.noreply.github.com>
2023-08-11 13:24:38 +1200

    Update game.py
    Finalized Checking for Winner
    Added Player turns
    Game is playable
    Game ends when 4 in a row is detected.

--- Code/game.py
+++ Code/game.py
@@ -3,58 +3,15 @@ Ultimate Connect.
 Creators: Jacob Purdue and Nick Kho
 Start date: Thursday 20th July
 """
-import pygame
 import sys
 import time
 
-# Intializes pygame
-pygame.init()
-
-screen_width = 800
-screen_height = 600
-screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("jacob stinks")
-
-button_width = 200
-button_height = 100
-button_color = (0, 255, 0)  # Green color (RGB)
-button_text_color = (255, 255, 255)  # White color (RGB)
-button_text_size = 30
-button_text = "Click Me!"
-
-def draw_button(x, y):
-    pygame.draw.rect(screen, button_color, (x, y, button_width, button_height))
-    font = pygame.font.SysFont(None, button_text_size)
-    text = font.render(button_text, True, button_text_color)
-    text_rect = text.get_rect(center=(x + button_width / 2, y + button_height / 2))
-    screen.blit(text, text_rect)
-
-while True:
-    break
-    # ^ This stops the gui from being run
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            mouse_x, mouse_y = pygame.mouse.get_pos()
-            if x < mouse_x < x + button_width and y < mouse_y < y + button_height:
-                # Do something when the button is clicked
-                print("Button clicked!")
-
-    screen.fill((255, 255, 255))  # Fill the screen with white color
-    x = (screen_width - button_width) // 2
-    y = (screen_height - button_height) // 2
-    draw_button(x, y)
-    pygame.display.flip()
-
 grid = []
-for y in range(12):
+for y in range(6):
 
     grid_list = []
 
-    for x in range(14):
+    for x in range(7):
         grid_list.append(0)
 
     grid.append(grid_list)
@@ -154,12 +111,17 @@ def checkDiagonallyOpposite(row, col, player, checkNumber):
     # Check Number = 4
     # if max - row (10) <= (10) max - checkNumber:
     #   run
-    if row > rows - checkNumber or col > cols - checkNumber:
+    if row > rows - checkNumber or col < 4:
         return False
 
     # Check the four diagonal squares in the bottom-right direction
     # and return the result.
-    return all(grid[row+i][col+i] == player for i in range(checkNumber))
+    # for i in range(checkNumber):
+    #     print(row+i, col-i, grid[row+i][col-i], i)
+    # print(grid[8][4])
+    # print(grid[9][3])
+    # print(all(grid[row+i][col-i] == player for i in range(checkNumber)))
+    return all(grid[row+i][col-i] == player for i in range(checkNumber))
 
 
 def checkDiagonally(row, col, player, checkNumber):
@@ -172,6 +134,7 @@ def checkDiagonally(row, col, player, checkNumber):
 
     # Check the four diagonal squares in the bottom-right direction
     # and return the result.
+
     return all(grid[row+i][col+i] == player for i in range(checkNumber))
 
 
@@ -218,7 +181,7 @@ def checkWinner(checkNumber):
                 totaltimes += 1
                 players = [1, 2]
                 for player in players:
-                    if grid[row][col] != 0:
+                    if grid[row][col] is player:
                         # Check diagonally, horizontally, and vertically.
                         if (player if checkInRow(row, col, player, checkNumber) else 0):
                             return player
@@ -231,6 +194,9 @@ def checkWinner(checkNumber):
                         # winnerFoundBool = True if (winnerFound != 0) else 0
                         if (player if checkUnderneath(row, col, player, checkNumber) else 0):
                             return player
+
+                        if (player if checkDiagonallyOpposite(row, col, player, checkNumber) else 0):
+                            return player
                         # winnerFoundBool = True if (winnerFound != 0) else 0
         return winnerFound
     return winnerFound
@@ -240,17 +206,41 @@ def place(row, collumn, player):
     grid[row][collumn] = player
 
 
+def clearTerminal():
+    for i in range(20):
+        print("\n")
+
+
+def playerTurnLoop():
+    PLAYERS = [1, 2]
+    gameOver = False
+    while gameOver == False:
+        for player in PLAYERS:
+            chosenSquare = False
+            while chosenSquare is False:
+                try:
+                    clearTerminal()
+                    printGrid()
+                    desiredPlacement = int(input("Player {} turn.\nType a collumn to drop in ({})".format(player, ("1-" + str(len(grid[0]))))))
+                    desiredPlacement -= 1
+                    # print(desiredPlacement)
+                    # print(0 <= desiredPlacement)
+                    # print(desiredPlacement <= len(grid[0]))
+                    if 0 <= desiredPlacement and desiredPlacement <= len(grid[0]):
+                        chosenSquare = True
+                except:
+                    pass
+            putCounter(desiredPlacement, player)
+            gameOver = True if checkWinner(4) else False
+            if gameOver == True:
+                clearTerminal()
+                printGrid()
+                print("Game Over! {} Wins.".format(player))
+                return True
 # putCounter(0, 1)
-putCounter(1, 1)
-putCounter(2, 1)
-putCounter(0, 1)
-putCounter(0, 1)
-putCounter(0, 1)
-putCounter(2, 1)
-# putCounter(3, 1)
-putCounter(1, 1)
-putCounter(1, 1)
 # checkSurroundings(11, 1, 1)
-print(grid[11][0])
 printGrid()
 print("Winner Found" if checkWinner(4) else "Nothing Found")
+playerTurnLoop()
+# checkDiagonallyOpposite(8, 4, 1, 4)
+# checkDiagonallyOpposite(0, 0, 1, 4)
