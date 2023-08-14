35b8d0db47ecc628de3bf03574fe4e9361174652
Jacob Purdue <jacob.purdue@student.onslow.school.nz>
2023-08-14 21:07:42 +1200

    Finished board size selecting and resoltion adaptation. Also rearranged order of functions to be more logical.

--- Code/view.py
+++ Code/view.py
@@ -14,204 +14,15 @@ BOARD8X7 = pygame.image.load("../assets/Board8x7.drawio.png")
 BOARD9X8 = pygame.image.load("../assets/Board9x8.drawio.png")
 BOARD10X9 = pygame.image.load("../assets/Board10x9.drawio.png")
 BOARD11X10 = pygame.image.load("../assets/Board11x10.drawio.png")
+boards = []
 resolutions = [(811, 711), (911, 811), (1011, 911), (1111, 1011)]
 board = int
-
+boards.extend([BOARD8X7, BOARD9X8, BOARD10X9, BOARD11X10])
+print(boards)
 
 def get_font(size):  # Returns font in the desired size
 
     return pygame.font.Font("../assets/DiloWorld-mLJLv.ttf", size)
-
-
-def res_change(b_res):
-    # Change resolution
-    print(b_res)
-    new_resolution = resolutions[b_res]
-    SCREEN = pygame.display.set_mode(new_resolution)
-    current_resolution = new_resolution
-
-def game(board):
-    """Plays the game itself"""
-    if board is None:
-        board, b_res = board_size()
-    else:
-        b_res = board_size()[1]
-        res_change(b_res)
-        while True:
-            GAME_MOUSE_POS = pygame.mouse.get_pos()
-            SCREEN.fill("black")
-            SCREEN.blit(BOARD9X8, (0, 0))
-
-            # Return to main menu.
-            TEMP_BACK = Button(image=None, pos=(640, 460),
-                               text_input="BACK", font=get_font(75), base_color="White", hovering_color="Green")
-            TEMP_BACK.changeColor(GAME_MOUSE_POS)
-            TEMP_BACK.update(SCREEN)
-
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
-
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    if TEMP_BACK.checkForInput(GAME_MOUSE_POS):
-                        main_menu()
-
-            pygame.display.update()
-
-def play():
-    """Puts user into the start game menu"""
-    while True:
-
-        PLAY_MOUSE_POS = pygame.mouse.get_pos()
-
-        # Fills screen with black to place anything over.
-        SCREEN.fill("black")
-
-        # Main header text.
-        PLAY_TEXT = get_font(45).render("Game Setup.", True, "White")
-        PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 60))
-        SCREEN.blit(PLAY_TEXT, PLAY_RECT)
-
-        # Return to main menu.
-        PLAY_BACK = Button(image=None, pos=(640, 460),
-                           text_input="BACK", font=get_font(75), base_color="White", hovering_color="Green")
-
-        PLAY_BACK.changeColor(PLAY_MOUSE_POS)
-        PLAY_BACK.update(SCREEN)
-
-        # Continue to game
-        START_GAME = Button(image=None, pos=(640, 160),
-                            text_input="Start game", font=get_font(75), base_color="White", hovering_color="Green")
-
-        # Highlights options text while mouse hovers
-        START_GAME.changeColor(PLAY_MOUSE_POS)
-        START_GAME.update(SCREEN)
-
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if PLAY_BACK.checkForInput(PLAY_MOUSE_POS):
-                    main_menu()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if START_GAME.checkForInput(PLAY_MOUSE_POS):
-                    game(board, b_res)
-
-        pygame.display.update()
-
-def board_size():
-    while True:
-        # Gets mouse position
-        BOARD_MOUSE_POS = pygame.mouse.get_pos()
-
-        SCREEN.fill("white")
-
-        # Main header text.
-        BOARD_TEXT = get_font(45).render("Select a board size.", True, "Black")
-        BOARD_RECT = BOARD_TEXT.get_rect(center=(640, 60))
-        SCREEN.blit(BOARD_TEXT, BOARD_RECT)
-
-        # Button to return to play from board.
-        BOARD_BACK = Button(image=None, pos=(640, 460),
-                              text_input="BACK", font=get_font(75), base_color="Black", hovering_color="Green")
-
-        # Highlights options text while mouse hovers
-        BOARD_BACK.changeColor(BOARD_MOUSE_POS)
-        BOARD_BACK.update(SCREEN)
-
-        # Button to return to main menu from options.
-        eight_by_seven = Button(image=None, pos=(640, 160),
-                              text_input="8x7", font=get_font(75), base_color="Black", hovering_color="Green")
-
-        # Highlights text while mouse hovers
-        eight_by_seven.changeColor(BOARD_MOUSE_POS)
-        eight_by_seven.update(SCREEN)
-
-        # Button to return to main menu from options.
-        nine_by_eight = Button(image=None, pos=(640, 260),
-                                text_input="9x8", font=get_font(75), base_color="Black", hovering_color="Green")
-
-        # Highlights text while mouse hovers
-        nine_by_eight.changeColor(BOARD_MOUSE_POS)
-        nine_by_eight.update(SCREEN)
-
-        # Button to return to main menu from options.
-        ten_by_nine = Button(image=None, pos=(640, 360),
-                               text_input="10x9", font=get_font(75), base_color="Black", hovering_color="Green")
-
-        # Highlights text while mouse hovers
-        ten_by_nine.changeColor(BOARD_MOUSE_POS)
-        ten_by_nine.update(SCREEN)
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if eight_by_seven.checkForInput(BOARD_MOUSE_POS):
-                    board = BOARD8X7
-                    b_res = 0
-                    print(b_res)
-                    return board, b_res
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if nine_by_eight.checkForInput(BOARD_MOUSE_POS):
-                    board = BOARD9X8
-                    b_res = 1
-                    print(b_res)
-                    return board, b_res
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if ten_by_nine.checkForInput(BOARD_MOUSE_POS):
-                    board = BOARD10X9
-                    b_res = 2
-                    print(b_res)
-                    return board, b_res
-
-        pygame.display.update()
-
-
-def options():
-    """Allows the user to change some settings."""
-    while True:
-        # Gets mouse position
-        OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
-
-        SCREEN.fill("white")
-
-        # Main header text.
-        OPTIONS_TEXT = get_font(45).render("This is the OPTIONS screen.", True, "Black")
-        OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=(640, 260))
-        SCREEN.blit(OPTIONS_TEXT, OPTIONS_RECT)
-
-        # Button to return to main menu from options.
-        OPTIONS_BACK = Button(image=None, pos=(640, 460),
-                              text_input="BACK", font=get_font(75), base_color="Black", hovering_color="Green")
-
-        # Highlights options text while mouse hovers
-        OPTIONS_BACK.changeColor(OPTIONS_MOUSE_POS)
-        OPTIONS_BACK.update(SCREEN)
-
-        # Select board size
-        size_but = Button(image=None, pos=(640, 360),
-                          text_input="Choose Board Size", font=get_font(75), base_color="Black", hovering_color="Green")
-        size_but.changeColor(OPTIONS_MOUSE_POS)
-        size_but.update(SCREEN)
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if OPTIONS_BACK.checkForInput(OPTIONS_MOUSE_POS):
-                    main_menu()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if size_but.checkForInput(OPTIONS_MOUSE_POS):
-                    board_size()
-
-        pygame.display.update()
-
 
 def main_menu():
     """Main menu for the game."""
@@ -261,5 +72,191 @@ def main_menu():
 
         pygame.display.update()
 
+
+def play():
+    """Puts user into the start game menu"""
+    while True:
+
+        PLAY_MOUSE_POS = pygame.mouse.get_pos()
+
+        # Fills screen with black to place anything over.
+        SCREEN.fill("black")
+
+        # Main header text.
+        PLAY_TEXT = get_font(45).render("Game Setup.", True, "White")
+        PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 60))
+        SCREEN.blit(PLAY_TEXT, PLAY_RECT)
+
+        # Return to main menu.
+        PLAY_BACK = Button(image=None, pos=(640, 460),
+                           text_input="BACK", font=get_font(75), base_color="White", hovering_color="Green")
+
+        PLAY_BACK.changeColor(PLAY_MOUSE_POS)
+        PLAY_BACK.update(SCREEN)
+
+        # Continue to game
+        START_GAME = Button(image=None, pos=(640, 160),
+                            text_input="Start game", font=get_font(75), base_color="White", hovering_color="Green")
+
+        # Highlights options text while mouse hovers
+        START_GAME.changeColor(PLAY_MOUSE_POS)
+        START_GAME.update(SCREEN)
+
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if PLAY_BACK.checkForInput(PLAY_MOUSE_POS):
+                    main_menu()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if START_GAME.checkForInput(PLAY_MOUSE_POS):
+                    game(board)
+
+        pygame.display.update()
+
+def game(board):
+    """Plays the game itself"""
+    b_res = board_size()
+    board = boards[b_res]
+    res_change(b_res)
+    while True:
+        GAME_MOUSE_POS = pygame.mouse.get_pos()
+        SCREEN.fill("black")
+        SCREEN.blit(board, (0, 0))
+
+        # Return to main menu.
+        TEMP_BACK = Button(image=None, pos=(640, 460),
+                           text_input="BACK", font=get_font(75), base_color="White", hovering_color="Green")
+        TEMP_BACK.changeColor(GAME_MOUSE_POS)
+        TEMP_BACK.update(SCREEN)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if TEMP_BACK.checkForInput(GAME_MOUSE_POS):
+                    main_menu()
+
+        pygame.display.update()
+
+def board_size():
+    while True:
+        # Gets mouse position
+        BOARD_MOUSE_POS = pygame.mouse.get_pos()
+
+        SCREEN.fill("white")
+
+        # Main header text.
+        BOARD_TEXT = get_font(45).render("Select a board size.", True, "Black")
+        BOARD_RECT = BOARD_TEXT.get_rect(center=(640, 60))
+        SCREEN.blit(BOARD_TEXT, BOARD_RECT)
+
+        # Button to return to play from board.
+        BOARD_BACK = Button(image=None, pos=(640, 560),
+                              text_input="BACK", font=get_font(75), base_color="Black", hovering_color="Green")
+
+        # Highlights options text while mouse hovers
+        BOARD_BACK.changeColor(BOARD_MOUSE_POS)
+        BOARD_BACK.update(SCREEN)
+
+        # Button to return to main menu from options.
+        eight_by_seven = Button(image=None, pos=(640, 160),
+                              text_input="8x7", font=get_font(75), base_color="Black", hovering_color="Green")
+
+        # Highlights text while mouse hovers
+        eight_by_seven.changeColor(BOARD_MOUSE_POS)
+        eight_by_seven.update(SCREEN)
+
+        # Button to return to main menu from options.
+        nine_by_eight = Button(image=None, pos=(640, 260),
+                                text_input="9x8", font=get_font(75), base_color="Black", hovering_color="Green")
+
+        # Highlights text while mouse hovers
+        nine_by_eight.changeColor(BOARD_MOUSE_POS)
+        nine_by_eight.update(SCREEN)
+
+        # Button to return to main menu from options.
+        ten_by_nine = Button(image=None, pos=(640, 360),
+                               text_input="10x9", font=get_font(75), base_color="Black", hovering_color="Green")
+
+        # Highlights text while mouse hovers
+        ten_by_nine.changeColor(BOARD_MOUSE_POS)
+        ten_by_nine.update(SCREEN)
+
+        eleven_by_ten = Button(image=None, pos=(640, 460),
+                             text_input="11x10", font=get_font(75), base_color="Black", hovering_color="Green")
+
+        # Highlights text while mouse hovers
+        eleven_by_ten.changeColor(BOARD_MOUSE_POS)
+        eleven_by_ten.update(SCREEN)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if eight_by_seven.checkForInput(BOARD_MOUSE_POS):
+                    b_res = 0
+                    return b_res
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if nine_by_eight.checkForInput(BOARD_MOUSE_POS):
+                    b_res = 1
+                    return b_res
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if ten_by_nine.checkForInput(BOARD_MOUSE_POS):
+                    b_res = 2
+                    return b_res
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if eleven_by_ten.checkForInput(BOARD_MOUSE_POS):
+                    b_res = 3
+                    return b_res
+
+        pygame.display.update()
+
+def res_change(b_res):
+    # Change resolution
+    new_resolution = resolutions[b_res]
+    SCREEN = pygame.display.set_mode(new_resolution)
+    current_resolution = new_resolution
+
+
+def options():
+    """Allows the user to change some settings."""
+    while True:
+        # Gets mouse position
+        OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
+
+        SCREEN.fill("white")
+
+        # Main header text.
+        OPTIONS_TEXT = get_font(45).render("This is the OPTIONS screen.", True, "Black")
+        OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=(640, 260))
+        SCREEN.blit(OPTIONS_TEXT, OPTIONS_RECT)
+
+        # Button to return to main menu from options.
+        OPTIONS_BACK = Button(image=None, pos=(640, 460),
+                              text_input="BACK", font=get_font(75), base_color="Black", hovering_color="Green")
+
+        # Highlights options text while mouse hovers
+        OPTIONS_BACK.changeColor(OPTIONS_MOUSE_POS)
+        OPTIONS_BACK.update(SCREEN)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if OPTIONS_BACK.checkForInput(OPTIONS_MOUSE_POS):
+                    main_menu()
+
+
+        pygame.display.update()
 
 main_menu()
